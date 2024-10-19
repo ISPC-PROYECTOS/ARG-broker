c9a8dfd43e890fcc7d78b81b1287d76859cc485a
Victoria <vpicco1996@gmail.com>
2024-10-19 14:55:25 -0300

    Eliminado de carpete erronea

--- ulises/cuenta.py
+++
@@ -1,9 +0,0 @@
-class cuenta:
-    def __init__(self, titular, cantidad):
-        self.titular = titular
-        self.cantidad = cantidad
-
-    def mostrar(self):
-        print("Titular: ", self.titular)
-        print("Cantidad: ", self.cantidad)  
-        
